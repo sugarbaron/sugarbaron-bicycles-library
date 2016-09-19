2888faee128dda3db575e3943c9bbab8e2218939
sugarbaron <sugarbaron1@mail.ru>
2016-09-19 10:27:46 +0600

    repository adapted for building *.jar changes: 01.created manifest.mf, sugarbaron_library.jar 02.refactored external_code_emulator. here must not be any packages, because any public members became a library interface elements

--- source_code/z_external_code_emulator/ExitState.java
+++ source_code/z_external_code_emulator/ExitState.java
@@ -1,6 +1,6 @@
 /* author: sugarbaron ([sugarbaron_bicycles] e-mail:sugarbaron1@mail.ru)
    date: 01.09.2016 */
-package z_external_code_emulator.state_machine;
+package z_external_code_emulator;
 
 //[my bicycles]
 import ru.sugarbaron_bicycles.library.state_machine.*;
